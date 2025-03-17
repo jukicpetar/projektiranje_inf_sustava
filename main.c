81b9d4aa1af0c2d0c161e15f89050aa936d7eeb3
LJerkovic <ljerko01@gmail.com>
2025-03-17 18:23:40 +0100

    prvi upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main()
+{
+
+int broj1, broj2;
+printf("Unesite 2 broja koja će se zbrojiti");
+scanf("%d",&broj1);
+scanf("%d",&broj2);
+
+printf("%d",broj1*broj2); //namjeran bug/greška
+
+
+
+}
